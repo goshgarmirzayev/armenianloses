ca164cd7435270971f74a09ad0010a1f094b6908
Qoshqar R. Mirzayev <QRMirzayev@bankofbaku.com>
2020-10-29 12:41:11 +0400

    Changes

--- src/main/java/az/onbir/tv/IndexController.java
+++ src/main/java/az/onbir/tv/IndexController.java
@@ -1,11 +1,14 @@
 package az.onbir.tv;
 
 import az.onbir.tv.dao.ManDao;
+import az.onbir.tv.entity.Man;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 
@@ -18,13 +21,14 @@ public class IndexController {
 
     @GetMapping
     public ModelAndView index(ModelAndView modelAndView) {
-        modelAndView.addObject("mans", manDao.findAll());
+        modelAndView.addObject("mans", (List<Man>) manDao.findAll());
         modelAndView.setViewName("index");
         return modelAndView;
     }
+
     @RequestMapping(value = "/search")
     public ModelAndView search(ModelAndView modelAndView, @RequestParam String info) {
-        modelAndView.addObject("mans", manDao.findAllByInfoContaining(info));
+        modelAndView.addObject("mans", (List<Man>)manDao.findAllByInfoContaining(info));
         modelAndView.setViewName("index");
         return modelAndView;
     }
